5e8f3b6e2a73ee6c99e6356fe3abeca0f7c56905
Gabriel Victoria <81654814+gabrielvictoria96@users.noreply.github.com>
2021-10-25 12:53:40 -0300

    create webpage

---
+++ src/js/mobileMenu.js
@@ -0,0 +1,16 @@
+
+const mobileButton = document.querySelector(".btn-mobile");
+const navLinks = document.querySelector(".navlinks");
+
+mobileButton.addEventListener("click", openLinks);
+
+function openLinks() {
+   mobileButton.classList.toggle("open");
+   navLinks.classList.toggle("open");
+}
+
+window.addEventListener('scroll', () => {
+   const stickNav = document.querySelector(".navbar");
+
+   stickNav.classList.toggle("sticky", window.scrollY > 150);
+})
